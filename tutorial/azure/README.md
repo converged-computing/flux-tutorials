debde3e7a10df97b38a9c5145f5831b44d29a788
vsoch <vsoch@users.noreply.github.com>
2024-12-27 10:25:38 -0700

    tf: testing terraform setup for vmset
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ tutorial/azure/README.md
@@ -0,0 +1,48 @@
+# Flux on Azure
+
+## Usage
+
+### 1. Build Images
+
+Note that you should [build](build) the images first. Follow the instructions in the README there.
+
+### 2. Deploy Terraform
+
+Check the [start-script.sh](start-script.sh) and variables at the top of [main.tf](main.tf) and then:
+
+```bash
+make
+```
+Note that I needed to clone this and do from the cloud shell in the Azure portal.
+
+```bash
+git clone https://github.com/converged-computing/flux-tutorials
+cd flux-tutorials/tutorial/azure
+```
+
+You can also run each command separately:
+
+```
+# Terraform init
+make init
+
+# Terraform validate
+make validate
+
+# Create
+make apply
+
+# Destroy
+make destroy
+```
+
+### 3. Checks
+
+Check the cluster status, the overlay status, and try running a job:
+
+```bash
+$ flux resource list
+```
+```bash
+$ flux run -N 2 hostname
+```
--- tutorial/azure/main.tf
+++ tutorial/azure/main.tf
@@ -33,12 +33,35 @@ provider "azurerm" {
   }
 }
 
+locals {
+  admin_user     = "azureuser"
+  # How many VMs in the VM Scale set
+  size         = 2
+  name         = "flux"
+  disk_size_gb = 100
+  # TODO does it work if these are different (move across groups)
+  # Where we want to create the VM scale set
+  resource_group_name = "packer-testing"
+
+  # Custom Build variables (the packer build)
+  vm_image_name = "flux-framework"
+  # e.g., "/subscriptions/xxxxxxx/resourceGroups/xxxxx/providers/Microsoft.Compute/disks/xxxx"
+  # vm_image_storage_reference = env("AZURE_VM_IMAGE_STORAGE_REFERENCE")
+  vm_image_resource_group = "packer-testing"
+  vm_image_size           = "Standard_HB120-96rs_v3"
+  location                = "southcentralus"
+  tags = {
+    flux_core = "0-68-0"
+  }
+  application_port = 8081
+}
+
 resource "random_pet" "id" {}
 
 resource "azurerm_resource_group" "vmss" {
-  name     = coalesce(var.resource_group_name, "flux-${random_pet.id.id}")
-  location = var.location
-  tags     = var.tags
+  name     = coalesce(local.resource_group_name, "${local.name}-${random_pet.id.id}")
+  location = local.location
+  tags     = local.tags
 }
 
 resource "random_string" "fqdn" {
@@ -51,9 +74,9 @@ resource "random_string" "fqdn" {
 resource "azurerm_virtual_network" "vmss" {
   name                = "vmss-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  location            = local.location
   resource_group_name = azurerm_resource_group.vmss.name
-  tags                = var.tags
+  tags                = local.tags
 }
 
 resource "azurerm_subnet" "vmss" {
@@ -65,24 +88,23 @@ resource "azurerm_subnet" "vmss" {
 
 resource "azurerm_public_ip" "vmss" {
   name                = "vmss-public-ip"
-  location            = var.location
+  location            = local.location
   resource_group_name = azurerm_resource_group.vmss.name
   allocation_method   = "Static"
   domain_name_label   = random_string.fqdn.result
-  tags                = var.tags
+  tags                = local.tags
 }
 
 resource "azurerm_lb" "vmss" {
   name                = "vmss-lb"
-  location            = var.location
+  location            = local.location
   resource_group_name = azurerm_resource_group.vmss.name
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.vmss.id
   }
-
-  tags = var.tags
+  tags = local.tags
 }
 
 resource "azurerm_lb_backend_address_pool" "bpepool" {
@@ -93,26 +115,26 @@ resource "azurerm_lb_backend_address_pool" "bpepool" {
 resource "azurerm_lb_probe" "vmss" {
   loadbalancer_id = azurerm_lb.vmss.id
   name            = "ssh-running-probe"
-  port            = var.application_port
+  port            = local.application_port
 }
 
 resource "azurerm_lb_rule" "lbnatrule" {
   loadbalancer_id                = azurerm_lb.vmss.id
   name                           = "http"
   protocol                       = "Tcp"
-  frontend_port                  = var.application_port
-  backend_port                   = var.application_port
+  frontend_port                  = local.application_port
+  backend_port                   = local.application_port
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpepool.id]
   frontend_ip_configuration_name = "PublicIPAddress"
   probe_id                       = azurerm_lb_probe.vmss.id
 }
 
 data "azurerm_resource_group" "image" {
-  name = var.packer_resource_group_name
+  name = local.vm_image_resource_group
 }
 
 data "azurerm_image" "image" {
-  name                = var.packer_image_name
+  name                = local.vm_image_name
   resource_group_name = data.azurerm_resource_group.image.name
 }
 
@@ -124,56 +146,58 @@ resource "azapi_resource" "ssh_public_key" {
 }
 
 resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  resource_id = azapi_resource.ssh_public_key.id
-  action      = "generateKeyPair"
-  method      = "POST"
+  type                   = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  resource_id            = azapi_resource.ssh_public_key.id
+  action                 = "generateKeyPair"
+  method                 = "POST"
   response_export_values = ["publicKey", "privateKey"]
 }
 
 resource "random_password" "password" {
-  count  = var.admin_password == null ? 1 : 0
+  count  = 0
   length = 20
-}
-
-locals {
-  admin_password = try(random_password.password[0].result, var.admin_password)
 }
 
 resource "azurerm_virtual_machine_scale_set" "vmss" {
   name                = "vmscaleset"
-  location            = var.location
+  location            = local.location
   resource_group_name = azurerm_resource_group.vmss.name
   upgrade_policy_mode = "Manual"
+  custom_data         = file("start_script.sh")
 
   sku {
-    name     = "Standard_DS1_v2"
+    name     = "Standard_HB120-96rs_v3"
     tier     = "Standard"
-    capacity = 2
+    capacity = local.size
   }
 
   storage_profile_image_reference {
     id = data.azurerm_image.image.id
   }
 
+  # storage_image_reference {
+  #    id = local.vm_image_storage_reference
+  #  }
+
   storage_profile_os_disk {
-    name              = ""
-    caching           = "ReadWrite"
+    name              = "flux-framework"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
+    # disk_size_gb      = 75
   }
 
   storage_profile_data_disk {
+    # Logical unit of the disk in the scale set
     lun           = 0
     caching       = "ReadWrite"
     create_option = "Empty"
-    disk_size_gb  = 10
+    disk_size_gb  = local.disk_size_gb
   }
 
   os_profile {
     computer_name_prefix = "vmlab"
-    admin_username       = var.admin_user
-    admin_password       = local.admin_password
+    admin_username       = local.admin_user
+    admin_password       = null
   }
 
   os_profile_linux_config {
@@ -196,22 +220,21 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
       primary                                = true
     }
   }
-
-  tags = var.tags
+  tags = local.tags
 }
 
 resource "azurerm_public_ip" "flux" {
-  name                = "flux-public-ip"
-  location            = var.location
+  name                = "${local.name}-public-ip"
+  location            = local.location
   resource_group_name = azurerm_resource_group.vmss.name
   allocation_method   = "Static"
   domain_name_label   = "${random_string.fqdn.result}-ssh"
-  tags                = var.tags
+  tags                = local.tags
 }
 
 resource "azurerm_network_interface" "flux" {
-  name                = "flux-nic"
-  location            = var.location
+  name                = "${local.name}-nic"
+  location            = local.location
   resource_group_name = azurerm_resource_group.vmss.name
 
   ip_configuration {
@@ -220,45 +243,48 @@ resource "azurerm_network_interface" "flux" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.flux.id
   }
-
-  tags = var.tags
+  tags = local.tags
 }
 
-resource "azurerm_virtual_machine" "flux" {
-  name                  = "flux"
-  location              = var.location
-  resource_group_name   = azurerm_resource_group.vmss.name
-  network_interface_ids = [azurerm_network_interface.flux.id]
-  vm_size               = "Standard_DS1_v2"
+#resource "azurerm_virtual_machine" "flux" {
+#  name                  = local.vm_image_name
+#  location              = local.location
+#  resource_group_name   = azurerm_resource_group.vmss.name
+#  network_interface_ids = [azurerm_network_interface.flux.id]
+#  vm_size               = local.vm_image_size
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
+#  storage_image_reference {
+#    publisher = "Canonical"
+#    offer     = "UbuntuServer"
+#    sku       = "16.04-LTS"
+#    version   = "latest"
+#  }
 
-  storage_os_disk {
-    name              = "flux-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
+#  storage_os_disk {
+#    name              = "${local.name}-osdisk"
+#    caching           = "ReadWrite"
+#    create_option     = "FromImage"
+#    managed_disk_type = "Standard_LRS"
+#  }
 
-  os_profile {
-    computer_name  = "flux"
-    admin_username = var.admin_user
-    admin_password = local.admin_password
-  }
+# storage_image_reference {
+#    id = local.vm_image_storage_reference
+#  }
 
-  os_profile_linux_config {
-    disable_password_authentication = true
+#  os_profile {
+#    computer_name  = local.name
+#    admin_username = var.admin_user
+#    admin_password = local.admin_password
+#  }
 
-    ssh_keys {
-      path     = "/home/azureuser/.ssh/authorized_keys"
-      key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
-    }
-  }
+#  os_profile_linux_config {
+#    disable_password_authentication = true
 
-  tags = var.tags
-}
+#    ssh_keys {
+#      path     = "/home/azureuser/.ssh/authorized_keys"
+#      key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
+#    }
+#  }
+
+#  tags = local.tags
+#}
